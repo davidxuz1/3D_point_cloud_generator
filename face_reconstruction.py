ac415fa47ea0811424e38697e5b916efc8769b57
vladyskai <116801803+vladyskai@users.noreply.github.com>
2024-01-24 17:22:25 +0100

    Updated color and filtering

--- face_reconstruction.py
+++ face_reconstruction.py
@@ -7,6 +7,94 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pyvista as pv
 from plyfile import PlyData, PlyElement
+from scipy.stats import zscore # NEW LIB
+import re
+
+
+def remove_coordinate_outliers(points3D):
+    z_scores = np.abs(zscore(points3D, axis=0))
+
+    # Replace outlier coordinates with NaN
+    outlier_mask = z_scores >= 50
+    points3D[outlier_mask] = np.nan
+
+    return points3D
+
+
+def scale_coordinates(points3D, scale_x=(-1, 1), scale_y=(-1.25, 1.25), scale_z=(-1.3, 1.3)):
+    nan_mask = ~np.isnan(points3D)
+    scaled_points = np.full(points3D.shape, np.nan)
+
+    # Define the scale for each axis
+    scales = [scale_x, scale_y, scale_z]
+
+    for dim in range(points3D.shape[1]):
+        valid_values = points3D[nan_mask[:, dim], dim]
+        min_val = np.min(valid_values)
+        max_val = np.max(valid_values)
+        scale_min, scale_max = scales[dim]
+
+        # Perform scaling only on non-NaN values
+        scaled_points[nan_mask[:, dim], dim] = (valid_values - min_val) / (max_val - min_val) * (scale_max - scale_min) + scale_min
+
+    return scaled_points
+
+
+def select_images(folder_path):
+    # Sort the image files
+    image_files = sorted([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))], key=lambda x: int(re.findall("\d+", x)[0]))
+    total_images = len(image_files)
+
+    # Select the 5 most centered images
+    center_index = total_images // 2
+    centered_images = [image_files[max(0, center_index - 2)], image_files[max(0, center_index - 1)], image_files[center_index],\
+         image_files[min(total_images - 1, center_index + 1)], image_files[max(0, center_index + 2)]]
+
+    # Select the 4 fifthile images
+    quartile_step = total_images // 5
+    quartile_images = [image_files[quartile_step], image_files[2*quartile_step], image_files[3*quartile_step], image_files[4*quartile_step]]
+
+    # Combine all selected images and sort them
+    selected_images = centered_images + quartile_images
+    selected_images_sorted = sorted(selected_images, key=lambda x: int(re.findall("\d+", x)[0]))
+
+    return selected_images_sorted
+
+def get_keypoint_color(image, x, y):
+    # Assuming x, y are the coordinates of the keypoint
+    # Get the color at (x, y), making sure to convert from BGR to RGB
+    color_bgr = image[int(y), int(x), :]  # OpenCV uses BGR by default
+    color_rgb = color_bgr[::-1]  # Convert BGR to RGB
+    return color_rgb
+
+
+def interpolate_between_landmarks(landmarks, start_index, end_index, num_points=100):
+    # Linearly interpolate points between two landmarks
+    interpolated_points = []
+    start_point = landmarks[start_index]
+    end_point = landmarks[end_index]
+    for i in range(1, num_points + 1):
+        alpha = i / (num_points + 1)
+        x = (1 - alpha) * start_point[0] + alpha * end_point[0]
+        y = (1 - alpha) * start_point[1] + alpha * end_point[1]
+        interpolated_points.append((x, y))
+    return interpolated_points
+
+
+def calculate_eye_region_3D(mean_points3D, start_index, end_index, num_points=100):
+    # Get the 3D coordinates for the start and end points of the eye region
+    start_point_3D = mean_points3D[start_index]
+    end_point_3D = mean_points3D[end_index]
+    
+    # Interpolate points in 3D space between the start and end points
+    interpolated_points_3D = []
+    for i in range(1, num_points + 1):
+        alpha = i / (num_points + 1)
+        interpolated_point_3D = (1 - alpha) * start_point_3D + alpha * end_point_3D
+        interpolated_points_3D.append(interpolated_point_3D)
+    
+    return np.array(interpolated_points_3D)
+
 
 ###################################################################################################################################################
 # Esta clase 'PointCloudReconstructor' está diseñada para procesar un conjunto de imágenes y reconstruir una nube de puntos 3D utilizando OpenCV y MediaPipe.
@@ -17,51 +105,106 @@ from plyfile import PlyData, PlyElement
 # Requisitos:
 # - Directorio con imágenes.
 # - Matriz intrínseca de la cámara.
-# - Librerías Python: cv2 (OpenCV), mediapipe, numpy, matplotlib, pyvista, plyfile.
+# - Librerías Python: cv2 (OpenCV), mediapipe, numpy, matplotlib, pyvista, plyfile, scipy.
 ###################################################################################################################################################
 
+
 class PointCloudReconstructor:
-    def __init__(self, folder_path, camera_matrix, max_slope_ratio=0.1):
+    def __init__(self, folder_path, original_image_dir, camera_matrix, max_slope_ratio=0.1):
         # Inicialización de la clase con la ruta del folder, la matriz de la cámara y el ratio máximo de pendiente.
         self.folder_path = folder_path
+        self.original_image_dir = original_image_dir
         self.camera_matrix = camera_matrix
         self.max_slope_ratio = max_slope_ratio
-        self.face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5)
+        self.face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5) #refine_landmarks=True
 
     def process_images(self):
-        # Procesa las imágenes en el directorio especificado y reconstruye la estructura 3D.
-        image_files = sorted([f for f in os.listdir(self.folder_path) if os.path.isfile(os.path.join(self.folder_path, f))], key=lambda x: int(re.findall("\d+", x)[0]))
-        points3D = None
+        image_files = select_images(self.folder_path)
+        # Assume the central image is the one with the best view
+        central_image_path = os.path.join(self.original_image_dir, image_files[len(image_files) // 2])
+        central_image = cv2.imread(central_image_path)
+        points3D_list = []  # List to accumulate 3D points from each pair
 
-        # Procesamiento de cada par de imágenes para la reconstrucción 3D.
+        # Obtain landmarks and descriptors for the central image
+        gray_central = cv2.cvtColor(central_image, cv2.COLOR_BGR2GRAY)
+        landmarks_central = self.get_landmarks(gray_central)
+
+        # After getting landmarks_central
+        left_eye_interior_landmarks1 = interpolate_between_landmarks(landmarks_central, 161, 154)
+        left_eye_interior_landmarks2 = interpolate_between_landmarks(landmarks_central, 158, 144)
+        left_eye_interior_landmarks3 = interpolate_between_landmarks(landmarks_central, 157, 163)
+        right_eye_interior_landmarks1 = interpolate_between_landmarks(landmarks_central, 398, 466)
+        right_eye_interior_landmarks2 = interpolate_between_landmarks(landmarks_central, 385, 373)
+        right_eye_interior_landmarks3 = interpolate_between_landmarks(landmarks_central, 387, 380)
+
+
+        # Add the new landmarks for the eyes to the central landmarks
+        landmarks_central += left_eye_interior_landmarks1 + right_eye_interior_landmarks1\
+            + left_eye_interior_landmarks2 + right_eye_interior_landmarks2\
+            + left_eye_interior_landmarks3 + right_eye_interior_landmarks3
+
+        keypoints_central, descriptors_central = self.get_sift_descriptors(gray_central, landmarks_central)
+        colors_central = [get_keypoint_color(central_image, kp.pt[0], kp.pt[1]) for kp in keypoints_central]
+
+        
+
         for i in range(len(image_files) - 1):
             image1_path = os.path.join(self.folder_path, image_files[i])
             image2_path = os.path.join(self.folder_path, image_files[i + 1])
             image1 = cv2.imread(image1_path)
             image2 = cv2.imread(image2_path)
+            
             gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
             gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
             # Obtención de landmarks y descriptores SIFT.
             landmarks1 = self.get_landmarks(gray1)
             landmarks2 = self.get_landmarks(gray2)
+
             keypoints1, descriptors1 = self.get_sift_descriptors(gray1, landmarks1)
             keypoints2, descriptors2 = self.get_sift_descriptors(gray2, landmarks2)
+            
+            # Matching
             good_matches = self.find_good_matches(keypoints1, keypoints2, descriptors1, descriptors2)
-
-            if len(good_matches) > 20:
-                # Estimación de la matriz esencial y reconstrucción de la estructura 3D.
+ 
+            #if len(good_matches) > 20:
+            if len(keypoints1) != 0 and len(keypoints2) != 0:
                 E = self.estimate_essential_matrix(keypoints1, keypoints2, self.camera_matrix)
                 points3D = self.reconstruct_structure(E, keypoints1, keypoints2, self.camera_matrix)
-                break
+                if points3D is not None:
+                    points3D_with_nan = remove_coordinate_outliers(points3D)
+                    scaled_points = scale_coordinates(points3D_with_nan)
+                    points3D_list.append(scaled_points)
+                    #average_colors = [(c1 + c2) // 2 for c1, c2 in zip(colors1, colors2)]  # Average colors
+                    #colors_list.append(average_colors)
+                    print(image1_path, image2_path)
 
-        # Visualización y guardado de la nube de puntos y visualización del mallado.
-        if points3D is not None:
-            self.display_point_cloud(points3D)
-            self.save_point_cloud_to_ply(points3D, 'nube_de_puntos.ply')
-            self.display_mesh(points3D)
+        # Compute the mean of the 3D points if there are any reconstructions
+        if points3D_list:
+            # Assuming all points3D arrays are of the same shape
+            mean_points3D = np.nanmean(np.array(points3D_list), axis=0)
+
+            # Assuming you've already computed mean_points3D...
+            left_eye_points_3D_1 = calculate_eye_region_3D(mean_points3D, 161, 173)
+            left_eye_points_3D_2 = calculate_eye_region_3D(mean_points3D, 158, 144)
+            left_eye_points_3D_3 = calculate_eye_region_3D(mean_points3D, 157, 163)
+            right_eye_points_3D_1 = calculate_eye_region_3D(mean_points3D, 398, 466)
+            right_eye_points_3D_2 = calculate_eye_region_3D(mean_points3D, 385, 373)
+            right_eye_points_3D_3 = calculate_eye_region_3D(mean_points3D, 387, 380)
+
+            # Concatenate the eye points to the mean_points3D for visualization
+            visual_points3D = np.concatenate((mean_points3D, left_eye_points_3D_1, right_eye_points_3D_1,\
+                                              left_eye_points_3D_2, right_eye_points_3D_2,\
+                                                left_eye_points_3D_3, right_eye_points_3D_3,\
+                                              ), axis=0)
+
+            # Use colors from the central image directly
+            self.display_point_cloud(visual_points3D)
+            self.save_point_cloud_to_ply(visual_points3D, colors_central, 'nube_de_puntos.ply')
+            self.display_mesh(visual_points3D, colors_central)
         else:
             print("No se pudo reconstruir la estructura 3D.")
+
 
     def get_landmarks(self, image):
         # Procesa la imagen usando MediaPipe para detectar puntos faciales (landmarks).
@@ -127,16 +270,31 @@ class PointCloudReconstructor:
         ax.set_zlabel('Eje Z')
         plt.show()
 
-    def save_point_cloud_to_ply(self, points3D, filename):
-        # Prepara y guarda la nube de puntos en un archivo PLY.
-        vertex = np.array([(p[0], p[1], p[2]) for p in points3D], dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
+    def save_point_cloud_to_ply(self, points3D, colors, filename):
+        # Make sure colors are in the correct format (uint8)
+        colors = np.asarray(colors, dtype=np.uint8)
+        vertex = np.array([(p[0], p[1], p[2], c[0], c[1], c[2]) for p, c in zip(points3D, colors)], dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
         el = PlyElement.describe(vertex, 'vertex')
         PlyData([el], text=True).write(filename)
 
-    def display_mesh(self, points3D):
-        # Crea una malla a partir de la nube de puntos y la visualiza.
+    def display_mesh(self, points3D, colors):
+        # Create a mesh from the point cloud
         cloud = pv.PolyData(points3D)
+        colors = np.asarray(colors, dtype=np.uint8)
+        cloud['colors'] = colors
+
+        # Generate a surface mesh using Delaunay 2D
         surf = cloud.delaunay_2d()
-        surf.plot(show_edges=True)
+
+        # Apply a mesh smoothing filter
+        smooth_surf = surf.smooth(n_iter=75)  # Adjust n_iter for more or less smoothing n_iter=100
+
+        # Plot the smoothed surface with colors
+        plotter = pv.Plotter()
+        plotter.add_mesh(smooth_surf, show_edges=False, rgb=True, scalars='colors')
+        plotter.show()
+
+
+
 
 
